340e26e220631ef8fa133347b72114d9ca05ba77
DioLive Studio <dio@live.ru>
2016-04-20 01:27:15 +0300

    Add project files.

---
+++ src/DioLive.Triangle.BindingModels/UpdateRequest.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Newtonsoft.Json;
+
+namespace DioLive.Triangle.BindingModels
+{
+    public class UpdateRequest
+    {
+        [JsonConstructor]
+        public UpdateRequest(Guid id, float moveDirection, float? beaming = null)
+        {
+            this.Id = id;
+            this.MoveDirection = moveDirection;
+            this.Beaming = beaming;
+        }
+
+        public Guid Id { get; }
+
+        public float MoveDirection { get; }
+
+        public float? Beaming { get; }
+    }
+}
---
+++ src/DioLive.Triangle.ServerCore/Startup.cs
@@ -0,0 +1,37 @@
+﻿using System.Text;
+using DioLive.Triangle.BindingModels;
+using Microsoft.AspNet.Builder;
+using Microsoft.AspNet.Hosting;
+using Microsoft.AspNet.Http;
+using Microsoft.Extensions.DependencyInjection;
+using Newtonsoft.Json;
+
+namespace DioLive.Triangle.ServerCore
+{
+    public class Startup
+    {
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit http://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app)
+        {
+            app.UseIISPlatformHandler();
+
+            app.Run(async (context) =>
+            {
+                byte[] buffer = new byte[context.Request.ContentLength ?? 0];
+                await context.Request.Body.ReadAsync(buffer, 0, buffer.Length);
+                string body = Encoding.ASCII.GetString(buffer);
+                var updateRequest = JsonConvert.DeserializeObject<UpdateRequest>(body);
+                await context.Response.WriteAsync("Hello World!");
+            });
+        }
+
+        // Entry point for the application.
+        public static void Main(string[] args) => WebApplication.Run<Startup>(args);
+    }
+}

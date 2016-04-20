c7e37dbd3df11c5fc732001a8cc5e7015c26d16d
DioLive Studio <dio@live.ru>
2016-04-20 22:46:43 +0300

    Add TakeAll() to RequestPool

--- src/DioLive.Triangle.ServerCore/RequestPool.cs
+++ src/DioLive.Triangle.ServerCore/RequestPool.cs
@@ -1,9 +1,17 @@
 ﻿using System.Collections.Concurrent;
+using System.Collections.Generic;
 using DioLive.Triangle.BindingModels;
 
 namespace DioLive.Triangle.ServerCore
 {
     public class RequestPool : BlockingCollection<UpdateRequest>
     {
+        public IEnumerable<UpdateRequest> TakeAll()
+        {
+            while (this.Count > 0)
+            {
+                yield return this.Take();
+            }
+        }
     }
 }

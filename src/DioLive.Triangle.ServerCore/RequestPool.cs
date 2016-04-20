d0de17ee897eeb69a60d559f691b4f26aca85e48
DioLive Studio <dio@live.ru>
2016-04-20 15:27:34 +0300

    Implement basic request pool

---
+++ src/DioLive.Triangle.ServerCore/RequestPool.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections.Concurrent;
+using DioLive.Triangle.BindingModels;
+
+namespace DioLive.Triangle.ServerCore
+{
+    public class RequestPool : BlockingCollection<UpdateRequest>
+    {
+    }
+}
--- src/DioLive.Triangle.ServerCore/Startup.cs
+++ src/DioLive.Triangle.ServerCore/Startup.cs
@@ -1,4 +1,5 @@
-﻿using System.Text;
+﻿using System.Collections.Generic;
+using System.Text;
 using DioLive.Triangle.BindingModels;
 using Microsoft.AspNet.Builder;
 using Microsoft.AspNet.Hosting;
@@ -14,6 +15,7 @@ namespace DioLive.Triangle.ServerCore
         // For more information on how to configure your application, visit http://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddSingleton<RequestPool>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -21,13 +23,39 @@ namespace DioLive.Triangle.ServerCore
         {
             app.UseIISPlatformHandler();
 
+            app.MapGet("/state", GetState);
+            app.MapPost("/update", PostUpdate);
+
+            app.Run(async (context) =>
+            {
+                await context.Response.WriteAsync("Hello World!");
+            });
+        }
+
+        private void GetState(IApplicationBuilder app)
+        {
+            app.Run(async (context) =>
+            {
+                var requestPool = context.RequestServices.GetRequiredService<RequestPool>();
+                var updateRequests = new List<UpdateRequest>();
+                while (requestPool.Count > 0)
+                {
+                    updateRequests.Add(requestPool.Take());
+                }
+                await context.Response.WriteAsync(JsonConvert.SerializeObject(updateRequests.ToArray()), Encoding.ASCII);
+            });
+        }
+
+        private void PostUpdate(IApplicationBuilder app)
+        {
             app.Run(async (context) =>
             {
                 byte[] buffer = new byte[context.Request.ContentLength ?? 0];
                 await context.Request.Body.ReadAsync(buffer, 0, buffer.Length);
                 string body = Encoding.ASCII.GetString(buffer);
                 var updateRequest = JsonConvert.DeserializeObject<UpdateRequest>(body);
-                await context.Response.WriteAsync("Hello World!");
+                var requestPool = context.RequestServices.GetRequiredService<RequestPool>();
+                requestPool.Add(updateRequest);
             });
         }
 

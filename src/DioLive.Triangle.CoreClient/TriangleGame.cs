a5b1c806a327d4f66bb2cb4e709fbc5181a6ed1e
DioLive Studio <dio@live.ru>
2016-04-25 14:05:14 +0300

    Rewrite color parser

--- src/DioLive.Triangle.CoreClient/TriangleGame.cs
+++ src/DioLive.Triangle.CoreClient/TriangleGame.cs
@@ -158,7 +158,39 @@ namespace DioLive.Triangle.CoreClient
 
         private static Color ParseColor(string hexColor)
         {
-            return new Color { PackedValue = Convert.ToUInt32(hexColor, 16) };
+            byte r;
+            byte g;
+            byte b;
+            byte a;
+
+            switch (hexColor.Length)
+            {
+                case 8:
+                    a = Convert.ToByte(hexColor.Substring(0, 2), 16);
+                    r = Convert.ToByte(hexColor.Substring(2, 2), 16);
+                    g = Convert.ToByte(hexColor.Substring(4, 2), 16);
+                    b = Convert.ToByte(hexColor.Substring(6, 2), 16);
+                    break;
+
+                case 6:
+                    a = 255;
+                    r = Convert.ToByte(hexColor.Substring(0, 2), 16);
+                    g = Convert.ToByte(hexColor.Substring(2, 2), 16);
+                    b = Convert.ToByte(hexColor.Substring(4, 2), 16);
+                    break;
+
+                case 3:
+                    a = 255;
+                    r = Convert.ToByte(new string(hexColor[0],2), 16);
+                    g = Convert.ToByte(new string(hexColor[1],2), 16);
+                    b = Convert.ToByte(new string(hexColor[2],2), 16);
+                    break;
+
+                default:
+                    throw new ArgumentException($"Unknown color definition: {hexColor}", nameof(hexColor));
+            }
+
+            return new Color(r, g, b, a);
         }
     }
 }

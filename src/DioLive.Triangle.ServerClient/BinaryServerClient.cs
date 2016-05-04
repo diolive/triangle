822032b1b0060584f60cf9a8a0c0c6b34c389257
DioLive Studio <dio@live.ru>
2016-05-04 12:53:33 +0300

    Add BinaryServerClient

---
+++ src/DioLive.Triangle.ServerClient/BinaryServerClient.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using DioLive.Triangle.Protocol;
+using DioLive.Triangle.Protocol.Binary;
+
+namespace DioLive.Triangle.ServerClient
+{
+    public class BinaryServerClient : HttpServerClient
+    {
+        private static IProtocol protocol;
+
+        static BinaryServerClient()
+        {
+            protocol = new BinaryProtocol();
+        }
+
+        public BinaryServerClient(Uri serverUri, bool initialize = true)
+            : base(protocol, serverUri, initialize)
+        {
+        }
+    }
+}

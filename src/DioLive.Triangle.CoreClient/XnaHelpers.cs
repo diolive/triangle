dbc5c8a821a595c411f44e8cb4e700ee78072b44
DioLive Studio <dio@live.ru>
2016-05-19 15:09:14 +0300

    Update XNA color helpers

--- src/DioLive.Triangle.CoreClient/XnaHelpers.cs
+++ src/DioLive.Triangle.CoreClient/XnaHelpers.cs
@@ -7,38 +7,44 @@ namespace DioLive.Triangle.CoreClient
     {
         public static Color ParseColor(string hexColor)
         {
-            byte r;
-            byte g;
-            byte b;
-            byte a;
-
             switch (hexColor.Length)
             {
                 case 8:
-                    a = Convert.ToByte(hexColor.Substring(0, 2), 16);
-                    r = Convert.ToByte(hexColor.Substring(2, 2), 16);
-                    g = Convert.ToByte(hexColor.Substring(4, 2), 16);
-                    b = Convert.ToByte(hexColor.Substring(6, 2), 16);
-                    break;
+                    return ParseAARRGGBB(hexColor);
 
                 case 6:
-                    a = 255;
-                    r = Convert.ToByte(hexColor.Substring(0, 2), 16);
-                    g = Convert.ToByte(hexColor.Substring(2, 2), 16);
-                    b = Convert.ToByte(hexColor.Substring(4, 2), 16);
-                    break;
+                    return ParseRRGGBB(hexColor);
 
                 case 3:
-                    a = 255;
-                    r = Convert.ToByte(new string(hexColor[0], 2), 16);
-                    g = Convert.ToByte(new string(hexColor[1], 2), 16);
-                    b = Convert.ToByte(new string(hexColor[2], 2), 16);
-                    break;
+                    return ParseRGB(hexColor);
 
                 default:
                     throw new ArgumentException($"Unknown color definition: {hexColor}", nameof(hexColor));
             }
+        }
 
+        private static Color ParseRGB(string hexColor)
+        {
+            byte r = Convert.ToByte(new string(hexColor[0], 2), 16);
+            byte g = Convert.ToByte(new string(hexColor[1], 2), 16);
+            byte b = Convert.ToByte(new string(hexColor[2], 2), 16);
+            return new Color(r, g, b, 255);
+        }
+
+        private static Color ParseRRGGBB(string hexColor)
+        {
+            byte r = Convert.ToByte(hexColor.Substring(0, 2), 16);
+            byte g = Convert.ToByte(hexColor.Substring(2, 2), 16);
+            byte b = Convert.ToByte(hexColor.Substring(4, 2), 16);
+            return new Color(r, g, b, 255);
+        }
+
+        private static Color ParseAARRGGBB(string hexColor)
+        {
+            byte a = Convert.ToByte(hexColor.Substring(0, 2), 16);
+            byte r = Convert.ToByte(hexColor.Substring(2, 2), 16);
+            byte g = Convert.ToByte(hexColor.Substring(4, 2), 16);
+            byte b = Convert.ToByte(hexColor.Substring(6, 2), 16);
             return new Color(r, g, b, a);
         }
     }
